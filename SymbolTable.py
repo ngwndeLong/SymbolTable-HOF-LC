00ce6b66feed34eef867a1f422a4bd3fb3f7b03d
n-hnglong <lognnh2112@gmail.com>
2025-04-30 13:11:24 +0700

    Upload source code without implement

---
+++ SymbolTable.py
@@ -0,0 +1,17 @@
+from StaticError import *
+from Symbol import *
+from functools import *
+
+
+def simulate(list_of_commands):
+    """
+    Executes a list of commands and processes them sequentially.
+
+    Args:
+        list_of_commands (list[str]): A list of commands to be executed.
+
+    Returns:
+        list[str]: A list of return messages corresponding to each command.
+    """ 
+    
+    return ["success", "success"]
