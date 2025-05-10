9a091365fe8c18af387635bb1143855c5c73d7d2
n-hnglong <lognnh2112@gmail.com>
2025-05-10 16:44:14 +0700

    finish the requirement

--- SymbolTable.py
+++ SymbolTable.py
@@ -2,7 +2,6 @@ from StaticError import *
 from Symbol import *
 from functools import *
 
-
 def simulate(list_of_commands):
     """
     Executes a list of commands and processes them sequentially.
@@ -12,6 +11,91 @@ def simulate(list_of_commands):
 
     Returns:
         list[str]: A list of return messages corresponding to each command.
-    """ 
+    """
+    def find(name, scopes):
+        return next(((i, scope[name]) for i, scope in reversed(list(enumerate(scopes))) if name in scope), None)
+
+    def valid_id(name):
+        return (len(name) > 0 and name[0].islower() and all(i.isalnum() or i == '_' for i in name))
     
-    return ["success", "success"]
+    def parse(cmd):
+        if cmd.strip() != cmd: raise InvalidInstruction(cmd)
+        if any(i == '' for i in cmd.split(' ')): raise InvalidInstruction(cmd)
+        return cmd.split(' ')
+    
+    def insert(cmd, parts, scopes):
+        if not valid_id(parts[1]) or parts[2] not in {"number", "string"}: raise InvalidInstruction(cmd)
+        if parts[1] in scopes[-1]: raise Redeclared(cmd)
+        return scopes[:-1] + [{**scopes[-1], parts[1]: parts[2]}], "success"
+    
+    def assign(cmd, parts, scopes):
+        if not valid_id(parts[1]): raise InvalidInstruction(cmd)
+        lhs = find(parts[1], scopes)
+        if not lhs: raise Undeclared(cmd)
+        rhs = parts[2]   
+        if rhs.isdigit():
+            if lhs[1] != "number": raise TypeMismatch(cmd)
+        elif len(rhs) >= 2 and rhs[0] == "'" and rhs[-1] == "'" and all(i.isalnum() for i in rhs[1:-1]):
+            if lhs[1] != "string": raise TypeMismatch(cmd)
+        elif valid_id(rhs):
+            rhs_found = find(rhs, scopes)
+            if not rhs_found: raise Undeclared(cmd)
+            if lhs[1] != rhs_found[1]: raise TypeMismatch(cmd)
+        else: raise InvalidInstruction(cmd)
+        return scopes[:-1] + [{**scopes[-1], parts[1]: rhs}], "success"
+    
+    def begin(scopes):
+        return scopes + [{}], None
+    
+    def end(scopes):
+        if len(scopes) == 1: raise UnknownBlock()
+        return scopes[:-1], None
+    
+    def lookup(cmd, parts, scopes):
+        if not valid_id(parts[1]): raise InvalidInstruction(cmd)
+        found = find(parts[1], scopes)
+        if not found: raise Undeclared(cmd)
+        return scopes, str(found[0])
+
+    def p_print(scopes):
+        sorted_vars = sorted(reduce(lambda acc, curr: acc if any(curr[0] == x[0] for x in acc) else acc + [curr], list((name, level) for level, scope in list(reversed(list(enumerate(scopes)))) for name in scope), []), key = lambda x: (x[1], list((name, level) for level, scope in list(reversed(list(enumerate(scopes)))) for name in scope).index(x)))
+        return scopes, " ".join(f"{name}//{level}" for name, level in sorted_vars)
+    
+    def r_print(scopes):
+        return scopes, " ".join(f"{name}//{level}" for name, level in reduce(lambda acc, curr: acc if any(curr[0] == x[0] for x in acc) else acc + [curr], list((name, level) for level, scope in list(reversed(list(enumerate(scopes)))) for name in reversed(scope)), []))
+    
+    def dispatch(cmd, parts, scopes):
+        if parts[0] == "INSERT":
+            if len(parts) != 3: raise InvalidInstruction(cmd)
+            return insert(cmd, parts, scopes)
+        elif parts[0] == "ASSIGN":
+            if len(parts) != 3: raise InvalidInstruction(cmd)
+            return assign(cmd, parts, scopes)
+        elif parts[0] == "BEGIN":
+            if len(parts) != 1: raise InvalidInstruction(cmd)
+            return begin(scopes)
+        elif parts[0] == "END": 
+            if len(parts) != 1: raise InvalidInstruction(cmd)
+            return end(scopes)
+        elif parts[0] == "LOOKUP": 
+            if len(parts) != 2: raise InvalidInstruction(cmd)
+            return lookup(cmd, parts, scopes)
+        elif parts[0] == "PRINT": 
+            if len(parts) != 1: raise InvalidInstruction(cmd)
+            return p_print(scopes)
+        elif parts[0] == "RPRINT":
+            if len(parts) != 1: raise InvalidInstruction(cmd)
+            return r_print(scopes)
+        else: raise InvalidInstruction(cmd)
+        
+    def step(acc, cmd):
+        current_output, current_scopes = acc
+        try:
+            parts = parse(cmd)
+            new_scopes, output = dispatch(cmd, parts, current_scopes)
+            return (current_output + [output], new_scopes)
+        except StaticError as e: raise e
+
+    final_output, final_scopes = reduce(step, list_of_commands, ([], [{}]))
+    if len(final_scopes) > 1: raise UnclosedBlock(len(final_scopes) - 1)
+    return list(filter(lambda x: x is not None, final_output))
--- TestSuite.py
+++ TestSuite.py
@@ -83,3 +83,459 @@ class TestSymbolTable(unittest.TestCase):
         expected = ["success", "success", "success", "success", "z//1 x//1 y//0"]
 
         self.assertTrue(TestUtils.check(input, expected, 106))
+
+    def test_7(self):
+        input = [
+            "    INSERT x number"
+        ]
+        expected = ["Invalid:     INSERT x number"]
+
+        self.assertTrue(TestUtils.check(input, expected, 107))
+
+    def test_8(self):
+        input = [
+            "INSERT x number",
+            "INSERT y string",
+            "BEGIN",
+            "INSERT x number",
+            "BEGIN",
+            "INSERT y string",
+            "END"
+        ]
+        expected = ["UnclosedBlock: 1"]
+
+        self.assertTrue(TestUtils.check(input, expected, 108))
+
+    def test_9(self):
+        input = [
+            "END",
+            "END",
+        ]
+        expected = ["UnknownBlock"]
+
+        self.assertTrue(TestUtils.check(input, expected, 109))
+
+    def test_10(self):
+        input = [
+            "INSERT x number",
+            "ASSIGN x y",
+        ]
+        expected = ["Undeclared: ASSIGN x y"]
+
+        self.assertTrue(TestUtils.check(input, expected, 110))
+
+    def test_11(self):
+        input = [
+            "INSERT x number",
+            "ASSIGN x x",
+        ]
+        expected = ["success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 111))
+
+    def test_12(self):
+        input = [
+            "INSERT x number",
+            "INSERT y string",
+            "BEGIN",
+            "INSERT x number",
+            "BEGIN",
+            "INSERT y string"
+        ]
+        expected = ["UnclosedBlock: 2"]
+
+        self.assertTrue(TestUtils.check(input, expected, 112))
+
+    def test_13(self):
+        input = [
+            "INSERT x string",
+            "ASSIGN x 'a@1'",
+        ]
+        expected = ["Invalid: ASSIGN x 'a@1'"]
+
+        self.assertTrue(TestUtils.check(input, expected, 113))
+
+    def test_14(self):
+        input = [
+            "BEGIN 3"
+        ]
+        expected = ["Invalid: BEGIN 3"]
+
+        self.assertTrue(TestUtils.check(input, expected, 114))
+
+    def test_15(self):
+        input = [
+            "INSERT abc 456   "
+        ]
+        expected = ["Invalid: INSERT abc 456   "]
+
+        self.assertTrue(TestUtils.check(input, expected, 115))
+
+    def test_16(self):
+        input = [
+            "INSERT x  string"
+        ]
+        expected = ["Invalid: INSERT x  string"]
+
+        self.assertTrue(TestUtils.check(input, expected, 116))
+
+    def test_17(self):
+        input = [
+            ""
+        ]
+        expected = ["Invalid: "]
+
+        self.assertTrue(TestUtils.check(input, expected, 117))
+
+    def test_18(self):
+        input = [
+            "INSERT a number",
+            "LOOKUP a"
+        ]
+        expected = ["success", "0"]
+
+        self.assertTrue(TestUtils.check(input, expected, 118))
+
+    def test_19(self):
+        input = [
+            "INSERT a number",
+            "BEGIN",
+            "INSERT b string",
+            "LOOKUP a",
+            "LOOKUP b",
+            "END",
+            "LOOKUP a"
+        ]
+        expected = ["success", "success", "0", "1", "0"]
+
+        self.assertTrue(TestUtils.check(input, expected, 119))
+
+    def test_20(self):
+        input = [
+            "INSERT a number",
+            "LOOKUP 1a"
+        ]
+        expected = ["Invalid: LOOKUP 1a"]
+
+        self.assertTrue(TestUtils.check(input, expected, 120))
+
+    def test_21(self):
+        input = [
+            "INSERT a number",
+            "LOOKUP b"
+        ]
+        expected = ["Undeclared: LOOKUP b"]
+
+        self.assertTrue(TestUtils.check(input, expected, 121))
+
+    def test_22(self):
+        input = [
+            "INSERT @@@ number",
+            "LOOKUP @@@"
+        ]
+        expected = ["Invalid: INSERT @@@ number"]
+
+        self.assertTrue(TestUtils.check(input, expected, 122))
+
+    def test_23(self):
+        input = [
+            "BEGIN",
+            "BEGIN",
+            "END",
+            "END"
+        ]
+        expected = []
+
+        self.assertTrue(TestUtils.check(input, expected, 123))
+
+    def test_24(self):
+        input = [
+            "INSERT number number",
+            "ASSIGN number number"
+        ]
+        expected = ["success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 124))
+
+    def test_25(self):
+        input = [
+            "INSERT number number",
+            "ASSIGN number number",
+            "INSERT string number",
+            "ASSIGN string number"
+        ]
+        expected = ["success", "success", "success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 125))
+
+    def test_26(self):
+        input = [
+            "INSERT number number",
+            "BEGIN",
+            "INSERT number string",
+            "END",
+            "ASSIGN number 1"
+        ]
+        expected = ["success", "success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 126))
+
+    def test_27(self):
+        input = [
+            "INSERT a number",
+            "INSERT b string",
+            "PRINT",
+            "RPRINT"
+        ]
+        expected = ["success", "success", "a//0 b//0", "b//0 a//0"]
+
+        self.assertTrue(TestUtils.check(input, expected, 127))
+
+    def test_28(self):
+        input = [
+            "INSERT a number",
+            "BEGIN",
+            "INSERT b string",
+            "PRINT",
+            "RPRINT",
+            "END",
+            "PRINT",
+            "RPRINT"
+        ]
+        expected = ["success", "success", "a//0 b//1", "b//1 a//0", "a//0", "a//0"]
+
+        self.assertTrue(TestUtils.check(input, expected, 128))
+
+    def test_29(self):
+        input = [
+            "INSERT x number",
+            "BEGIN",
+            "PRINT",
+            "END",
+            "LOOKUP y"
+        ]
+        expected = ["Undeclared: LOOKUP y"]
+
+        self.assertTrue(TestUtils.check(input, expected, 129))
+
+    def test_30(self):
+        input = [
+            "PRINT",
+            "RPRINT",
+            "BEGIN",
+            "END",
+            "PRINT",
+            "PRINT"
+        ]
+        expected = ["","","",""]
+
+        self.assertTrue(TestUtils.check(input, expected, 130))
+
+    def test_31(self):
+        input = [
+            "INSERT a1b2c3d4e5 string"
+        ]
+        expected = ["success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 131))
+
+    def test_32(self):
+        input = [
+            "LOOKUP xyz",
+            "LOOKUP def"
+        ]
+        expected = ["Undeclared: LOOKUP xyz"]
+
+        self.assertTrue(TestUtils.check(input, expected, 132))
+
+    def test_33(self):
+        input = [
+            "END",
+            "BEGIN",
+            "END",
+            "BEGIN"
+        ]
+        expected = ["UnknownBlock"]
+
+        self.assertTrue(TestUtils.check(input, expected, 133))
+
+    def test_34(self):
+        input = [
+            "BEGIN",
+            "END",
+            "BEGIN",
+            "END",
+            "BEGIN"
+        ]
+        expected = ["UnclosedBlock: 1"]
+
+        self.assertTrue(TestUtils.check(input, expected, 134))
+
+    def test_35(self):
+        input = [
+            "BEGIN",
+            "BEGIN",
+            "BEGIN",
+            "BEGIN",
+            "BEGIN"
+        ]
+        expected = ["UnclosedBlock: 5"]
+
+        self.assertTrue(TestUtils.check(input, expected, 135))
+
+    def test_36(self):
+        input = [
+            "INSERT z number",
+            "INSERT y string",
+            "INSERT x number",
+            "BEGIN",
+            "INSERT c number",
+            "INSERT b string",
+            "INSERT a number",
+            "PRINT",
+            "RPRINT"
+        ]
+        expected = ["UnclosedBlock: 1"]
+
+        self.assertTrue(TestUtils.check(input, expected, 136))
+
+    def test_37(self):
+        input = [
+            "INSERT z number",
+            "INSERT y string",
+            "INSERT x number",
+            "BEGIN",
+            "INSERT c number",
+            "INSERT b string",
+            "INSERT a number",
+            "PRINT",
+            "RPRINT",
+            "END"
+        ]
+        expected = ["success", "success", "success", "success", "success", "success", "z//0 y//0 x//0 c//1 b//1 a//1", "a//1 b//1 c//1 x//0 y//0 z//0"]
+
+        self.assertTrue(TestUtils.check(input, expected, 137))
+
+    def test_38(self):
+        input = [
+            "INSERT a1 string",
+            "ASSIGN a1 a1"
+        ]
+        expected = ["success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 138))
+
+    def test_39(self):
+        input = [
+            "INSERT a1 string",
+            "ASSIGN a1 'a1'"
+        ]
+        expected = ["success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 139))
+
+    def test_40(self):
+        input = [
+            "INSERT a1 number",
+            "ASSIGN a1 'a1'"
+        ]
+        expected = ["TypeMismatch: ASSIGN a1 'a1'"]
+
+        self.assertTrue(TestUtils.check(input, expected, 140))
+
+    def test_41(self):
+        input = [
+            "INSERT a1 string",
+            "ASSIGN a1 a2a3a4"
+        ]
+        expected = ["Undeclared: ASSIGN a1 a2a3a4"]
+
+        self.assertTrue(TestUtils.check(input, expected, 141))
+
+    def test_42(self):
+        input = [
+            "INSERT a1 string",
+            "ASSIGN a1 ''"
+        ]
+        expected = ["success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 142))
+
+    def test_43(self):
+        input = [
+            "INSERT a1 string",
+            "ASSIGN a1"
+        ]
+        expected = ["Invalid: ASSIGN a1"]
+
+        self.assertTrue(TestUtils.check(input, expected, 143))
+
+    def test_44(self):
+        input = [
+            "INSERT a1xyz number",
+            "ASSIGN a1xyz 555       "
+        ]
+        expected = ["Invalid: ASSIGN a1xyz 555       "]
+
+        self.assertTrue(TestUtils.check(input, expected, 144))
+
+    def test_45(self):
+        input = [
+            "INSERT 2345var000 number",
+            "LOOKUP 2345var000"
+        ]
+        expected = ["Invalid: INSERT 2345var000 number"]
+
+        self.assertTrue(TestUtils.check(input, expected, 145))
+
+    def test_46(self):
+        input = [
+            "INSERT def number",
+            " LOOKUP def"
+        ]
+        expected = ["Invalid:  LOOKUP def"]
+
+        self.assertTrue(TestUtils.check(input, expected, 146))
+
+    def test_47(self):
+        input = [
+            "INSERT x string",
+            "ASSIGN x ''''"
+        ]
+        expected = ["Invalid: ASSIGN x ''''"]
+
+        self.assertTrue(TestUtils.check(input, expected, 147))
+
+    def test_48(self):
+        input = [
+            "INSERT x2 string",
+            "ASSIGN x2 'done'",
+            "INSERT x2 string",
+            "ASSIGN x2 'done'",
+        ]
+        expected = ["Redeclared: INSERT x2 string"]
+
+        self.assertTrue(TestUtils.check(input, expected, 148))
+
+    def test_49(self):
+        input = [
+            "INSERT testcasecuoicung string",
+            "ASSIGN testcasecuoicung 'chucquythaycosuckhoe'",
+            "BEGIN",
+            "INSERT testcasecuoicung string",
+            "ASSIGN testcasecuoicung 'chucquythaycosuckhoe'",
+            "END"
+        ]
+        expected = ["success", "success", "success", "success"]
+
+        self.assertTrue(TestUtils.check(input, expected, 149))
+
+    
+    
+    
+
+    
+    
+
+    
+    
